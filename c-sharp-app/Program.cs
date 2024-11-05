dbc54d2849aac75976656c308e5323d1e09e0da8
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-11-05 14:15:09 +0100

    init.

---
+++ c-sharp-app/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.Contracts;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Assigment 1
+        // Console.Write("Enter a number: ");
+        // int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Console.Write("Enter a second number: ");
+        // int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Console.WriteLine(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+        // Console.WriteLine(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+        // Console.WriteLine(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
+
+        // Assigment 2
+        // Console.Write("Enter degrees in celsius: ");
+        // int celsius = Convert.ToInt32(Console.ReadLine());
+        // double farhenheit = 1.8 * celsius + 32;
+
+        // Console.WriteLine(celsius + "°C" + " = " + farhenheit + "°F");
+
+        // Assigment 3
+        // Console.WriteLine("Hello. Please answer following questions: ");
+        // Console.Write("First name: ");
+        // string? firstName = Console.ReadLine();
+
+        // Console.Write("Date of birth: ");
+        // int dateOfBirth = Convert.ToInt32(Console.ReadLine());
+        // Console.Write("Today's date: ");
+        // int currentDate = Convert.ToInt32(Console.ReadLine());
+
+        // int yearsOld = (currentDate - dateOfBirth) / 10000;
+
+        // Console.WriteLine("Hello " + firstName + ". " + "You are " + yearsOld + " old");
+    }
+} 
